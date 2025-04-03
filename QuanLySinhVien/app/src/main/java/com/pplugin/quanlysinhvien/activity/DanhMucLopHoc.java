326415a43941705fc4c2a31c8e77e041fa005cb0
pplugin <pluginldt@gmail.com>
2025-04-03 15:22:35 +0700

    Complete QuanLySinhVien demo

--- QuanLySinhVien/app/src/main/java/com/pplugin/quanlysinhvien/activity/DanhMucLopHoc.java
+++ QuanLySinhVien/app/src/main/java/com/pplugin/quanlysinhvien/activity/DanhMucLopHoc.java
@@ -1,6 +1,11 @@
 package com.pplugin.quanlysinhvien.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +14,59 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.pplugin.quanlysinhvien.R;
+import com.pplugin.quanlysinhvien.adapter.LopHocAdapter;
+import com.pplugin.quanlysinhvien.model.LopHoc;
+import com.pplugin.quanlysinhvien.sqlite.LopHocDAO;
 
-public class DanhMucLopHoc extends AppCompatActivity {
+import java.util.List;
 
+public class DanhMucLopHoc extends AppCompatActivity implements View.OnClickListener{
+    EditText edtTenLopHoc;
+    ListView lvDanhSachLopHoc;
+    List<LopHoc> lopHocList;
+    LopHocAdapter lopHocAdapter;
+    private void fillLopHocListView() {
+        LopHocDAO lopHocDAO = new LopHocDAO(this);
+        lopHocList = lopHocDAO.getAll();
+        lopHocAdapter = new LopHocAdapter(this, lopHocList);
+        lvDanhSachLopHoc.setAdapter(lopHocAdapter);
+        lvDanhSachLopHoc.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                LopHocDAO lopHocDAO1 = new LopHocDAO(DanhMucLopHoc.this);
+                LopHoc lopHoc = lopHocList.get(i);
+                lopHocDAO.delete(lopHoc.getId());
+                fillLopHocListView();
+                return false;
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLuuLopHoc:
+                LopHoc lopHoc = new LopHoc();
+                lopHoc.setTenlophoc(edtTenLopHoc.getText().toString());
+                LopHocDAO lopHocDAO = new LopHocDAO(this);
+                lopHocDAO.insert(lopHoc);
+                fillLopHocListView();
+                Toast.makeText(this,"Đã lưu lớp học",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnThoatLopHoc:
+                finish();
+                break;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_danh_muc_lop_hoc);
+        findViewById(R.id.btnLuuLopHoc).setOnClickListener(this);
+        findViewById(R.id.btnThoatLopHoc).setOnClickListener(this);
+        edtTenLopHoc = findViewById(R.id.edtTenLopHoc);
+        lvDanhSachLopHoc = findViewById(R.id.lvdanhsachlophoc);
+        fillLopHocListView();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- QuanLySinhVien/app/src/main/java/com/pplugin/quanlysinhvien/activity/QuanLySinhVien.java
+++ QuanLySinhVien/app/src/main/java/com/pplugin/quanlysinhvien/activity/QuanLySinhVien.java
@@ -1,6 +1,12 @@
 package com.pplugin.quanlysinhvien.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +15,101 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.pplugin.quanlysinhvien.R;
+import com.pplugin.quanlysinhvien.adapter.LopHocAdapter;
+import com.pplugin.quanlysinhvien.adapter.SinhVienAdapter;
+import com.pplugin.quanlysinhvien.helper.DateTimeHelper;
+import com.pplugin.quanlysinhvien.model.LopHoc;
+import com.pplugin.quanlysinhvien.model.SinhVien;
+import com.pplugin.quanlysinhvien.sqlite.LopHocDAO;
+import com.pplugin.quanlysinhvien.sqlite.SinhVienDAO;
 
-public class QuanLySinhVien extends AppCompatActivity {
+import java.util.List;
+
+public class QuanLySinhVien extends AppCompatActivity implements View.OnClickListener{
+    EditText edtMaSV, edtHotenSV, edtNgaySinhSV;
+    ListView lvDanhsachSinhvien;
+    Spinner spLopHoc ;
+    SinhVienAdapter sinhVienAdapter;
+    private List<LopHoc> lopHocList;
+    private List<SinhVien> sinhVienList;
+
+    @Override
+    public void onClick(View view) {
+        SinhVienDAO sinhVienDAO = new SinhVienDAO(this);
+        switch (view.getId()){
+            case R.id.btnLuuSinhVien:
+                try {
+                    SinhVien sinhVien = new SinhVien();
+                    sinhVien.setId(edtMaSV.getText().toString());
+                    sinhVien.setHoten(edtHotenSV.getText().toString());
+                    sinhVien.setNgaysinh(DateTimeHelper.toDate(edtNgaySinhSV.getText().toString()) );
+                    Toast.makeText(this,"AAA", Toast.LENGTH_LONG).show();
+                    int lopHoc = spLopHoc.getSelectedItemPosition();
+                    sinhVien.setLophocid(lopHocList.get(lopHoc).getId());
+                    Toast.makeText(this,"" + lopHocList.get(lopHoc).getId(),Toast.LENGTH_LONG).show();
+                    sinhVienDAO.insert(sinhVien);
+                    Toast.makeText(this,"Sinh viên đã được lưu",Toast.LENGTH_LONG).show();
+                    fillLopHocToListView();
+                } catch ( Exception ex )
+                {
+                    ex.printStackTrace();
+                }
+                break;
+
+            case R.id.btnThoatSinhVien:
+                finish();
+                break;
+        }
+    }
+
+    private void fillLopHocToListView() {
+        SinhVienDAO sinhVienDAO = new SinhVienDAO(this);
+        try {
+            int lopHocid =lopHocList.get(spLopHoc.getSelectedItemPosition()).getId() ;
+            sinhVienList = sinhVienDAO.getAllByLophoc(lopHocid);
+            sinhVienAdapter = new SinhVienAdapter(this,sinhVienList);
+            lvDanhsachSinhvien.setAdapter(sinhVienAdapter);
+        } catch ( Exception ex )
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    private void fillLopHocToSpinner(){
+        LopHocDAO lopHocDAO = new LopHocDAO(this);
+        lopHocList = lopHocDAO.getAll();
+        LopHocAdapter lopHocAdapter = new LopHocAdapter(this, lopHocList);
+        spLopHoc.setAdapter(lopHocAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quan_ly_sinh_vien);
+        findViewById(R.id.btnLuuSinhVien).setOnClickListener(this);
+        findViewById(R.id.btnThoatSinhVien).setOnClickListener(this);
+        edtMaSV = findViewById(R.id.edtMaSV);
+        edtHotenSV = findViewById(R.id.edtHotenSV);
+        edtNgaySinhSV = findViewById(R.id.edtNgaySinhSV);
+        spLopHoc = findViewById(R.id.spLopHoc);
+        fillLopHocToSpinner();
+        lvDanhsachSinhvien = findViewById(R.id.lvDanhsachSinhvien);
+        fillLopHocToListView();
+        spLopHoc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                fillLopHocToListView();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
